44cd3dcf1c1909fe4ebaeeb65a027cb6940c57ac
pritisharma1511 <ps0130815@gmail.com>
2025-11-01 11:12:47 +0530

    done

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import { useState } from 'react';
 import './App.css';
+
+const Create = () => {
+  const [task, setTask] = useState('');
+  const handleAdd = () => {
+    // Functionality to add a new task will be implemented here
+  };
+  return (
+    <div className="create">
+      <input type="text" placeholder="Enter task" value={task} onChange={(e) => setTask(e.target.value)} />
+      <button type="button" onClick={handleAdd}>Add</button>
+    </div>
+  );
+};
 
 function Create() {
   return (
     <div className="create">
-      <input type="text" placeholder="Enter a todo..." />
-      <button type="button">Add</button>
+      <input type="text" placeholder="Enter task" />
+      <button type="button" onClick={handleAdd}>Add</button>
     </div>
   );
 }
